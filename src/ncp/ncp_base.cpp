dcfd46c38e87228c88c0fbb19a13a68b44f64af8
Parag Balbudhe <parag.balbudhe@silabs.com>
2023-02-02 11:19:21 -0500

    Bug fixes...
    It fixes two bugs.
    1) In ncp_base.cpp, push the CurrentCommandIID to queue to be used by PandingEnergyScanHandler.
    2) In sub_mac.cpp, check for kStateDelayBeforeRetx when deciding if the device is transmitting or scanning. When there is an Ack Timeout, the RCP by default will try to re-transmit the failed packet, but is configured to give a random backoff during this period. When it does it will be in kStateDelayBeforeRetx state. If not careful, during the backoff period, RCP might accept another packet to transmit and face issues when sub_mac begins the transmission again.
    3) In radio_spinel_impl.hpp, when the RCP recovery begins, the SPINEL tries to move to Rx state, switch channels to initialise the RCP. But these should not be performed when running as Multiprotocol RCP. So, if the RCP throws an error, ignore it.
    4) In soft_source_match_table.c, detect if any duplicates are already present in source match table before adding a new entry. Before adding a new entry to source match table, try to find if any duplicate exists. If it does, return success, without trying to add a new entry. If not, continue adding the item to the table. This is an improvement rather than bug to avoid single child consuming multiple/all the entries from source match table.

--- src/ncp/ncp_base.cpp
+++ src/ncp/ncp_base.cpp
@@ -865,6 +865,9 @@ otError NcpBase::EnqueuePendingCommand(PendingCommandType aType, uint8_t aHeader
         break;
 
     case kPendingCommandTypeEnergyScan:
+        // We dont have access to header, but mCurCommandIID
+        // is updated in HandleReceive, use that instead.
+        entry->mIid         = mCurCommandIID;
         entry->mScanChannel = aScanChannel;
         break;
 
